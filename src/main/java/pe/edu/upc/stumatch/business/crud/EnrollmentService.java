bef55a78263d203a1cd7f14fdfe58bfb78ad37a4
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2022-06-29 12:53:18 -0500

    Agregar mensajes

--- src/main/java/pe/edu/upc/stumatch/business/crud/EnrollmentService.java
+++ src/main/java/pe/edu/upc/stumatch/business/crud/EnrollmentService.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 import pe.edu.upc.stumatch.model.entity.Enrollment;
 import pe.edu.upc.stumatch.model.entity.Section;
+import pe.edu.upc.stumatch.model.entity.Student;
 
 public interface EnrollmentService extends CrudService<Enrollment, Integer>{
 	public List<String[]> Reporte1();
 
 	public Integer insert(Enrollment enrollment);
+	
+	
+	public void NewVacancies(Section section,Enrollment enrollment);
+
+	public void UpdateCredit(Student student,Enrollment enrollment);
+	
 }
